fb68370e7857dac32d03597dbe7c06dd3e79b4a2
神楽坂碧玺 <0xbbc@0xbbc.com>
2016-10-07 01:29:31 +0800

    做了一些微小的工作
    - 可自动移动(step_time, 默认200ms)
    - 防止类似于直接从“向左”变到“向右”

--- Game.cpp
+++ Game.cpp
@@ -15,74 +15,72 @@
 //
 
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
 #include <termios.h>
+
+#ifdef __MACH__
+// macOS
+#include <sys/filio.h>
+#else
+// Linux
+#include <asm-generic/ioctls.h>
+#endif
 
 #include <chrono>
 #include <iostream>
 #include <thread>
 
-#include <Board.h>
+#include "Board.h"
 
 using std::cout;
 
 Board myBoard(15, 15);
+constexpr uint32_t step_time = 200;
+uint8_t currentInput = Up;
 
-uint8_t currentInput = -1;
-
-char getch() {
-    char buf = 0;
-    struct termios old = {0};
-    if (tcgetattr(0, &old) < 0) {
-        perror("tcsetattr()");
+void terminal(bool in_game) {
+    static struct termios old = {0};
+    struct termios game = {0};
+    if (in_game) {
+        if (tcgetattr(0, &old) < 0 || tcgetattr(0, &game) < 0) {
+            perror("tcsetattr()");
+        }
+        
+        game.c_lflag &= ~ICANON;
+        game.c_lflag &= ~ECHO;
+        game.c_lflag &= ~IEXTEN;
+        game.c_cc[VMIN] = 1;
+        game.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &game) < 0) {
+            perror("tcsetattr ICANON");
+        }
+    } else {
+        if (tcsetattr(0, TCSANOW, &old) < 0) {
+            perror("tcsetattr ICANON");
+        }
     }
-    old.c_lflag &= ~ICANON;
-    old.c_lflag &= ~ECHO;
-    old.c_cc[VMIN] = 1;
-    old.c_cc[VTIME] = 0;
-    if (tcsetattr(0, TCSANOW, &old) < 0) {
-        perror("tcsetattr ICANON");
-    }
-    if (read(0, &buf, 1) < 0) {
-        perror ("read()");
-    }
-    old.c_lflag |= ICANON;
-    old.c_lflag |= ECHO;
-    if (tcsetattr(0, TCSADRAIN, &old) < 0) {
-        perror ("tcsetattr ~ICANON");
-    }
-    return (buf);
 }
 
-void setup() {    
+void setup() {
     myBoard.setSnake(13, 7, Up);
     myBoard.setFood(1, 7);
     system("clear");
     myBoard.print();
+    terminal(true);
 }
 
 void gameover() {
+    terminal(false);
     exit(0);
 }
 
-int main() {
-    setup();
-    while (1) {
-        switch (getch()) {
-            case 'w':
-                currentInput = Up;
-                break;
-            case 's':
-                currentInput = Down;
-                break;
-            case 'a':
-                currentInput = Left;
-                break;
-            case 'd':
-                currentInput = Right;
-                break;
-            default:
-                break;
-        }
+void board_move_snake() {
+    static auto last = std::chrono::high_resolution_clock::now();
+    auto current = std::chrono::high_resolution_clock::now();
+    
+    fprintf(stderr, "%f\n", ((std::chrono::duration<double>)((current - last))).count());
+    if (((std::chrono::duration<double>)((current - last))).count() * 1000 > step_time) {
         switch (myBoard.moveSnake(currentInput)) {
             case -1:
                 gameover();
@@ -94,6 +92,72 @@ int main() {
             default:
                 break;
         }
+        last = std::chrono::high_resolution_clock::now();
+    }
+}
+
+int main() {
+    setup();
+    fd_set terminal_fd;
+    FD_ZERO(&terminal_fd);
+    FD_SET(STDIN_FILENO, &terminal_fd);
+    if (!FD_ISSET(STDIN_FILENO, &terminal_fd)) {
+        perror("Cannot select stdin");
+        exit(-1);
+    }
+    while (1) {
+        // timeout for select
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = step_time * 1000;
+        
+        // capture terminal_fd
+        fd_set terminal_read = terminal_fd;
+        
+        // wait for reading
+        int terminal_ret = select(FD_SETSIZE, &terminal_read, NULL, NULL, &timeout);
+        
+        // timeout
+        if (terminal_ret == 0) {
+        } else if (terminal_ret == -1) { // error
+            perror("[ERROR] select:");
+        } else { // read available
+            // read
+            if (FD_ISSET(0, &terminal_read)) {
+                // bytes
+                int count;
+                
+                // read through ioctl
+                ioctl(0, FIONREAD, &count);
+                
+                // there are some characters available to read
+                if (count == 1) {
+                    unsigned char input = '\0';
+                    
+                    // read from stdin
+                    count = (int)read(STDIN_FILENO, &input, count);
+                    if (count == 1) {
+                        switch (input) {
+                            case 'w':
+                                if (currentInput != Down) currentInput = Up;
+                                break;
+                            case 's':
+                                if (currentInput != Up) currentInput = Down;
+                                break;
+                            case 'a':
+                                if (currentInput != Right) currentInput = Left;
+                                break;
+                            case 'd':
+                                if (currentInput != Left) currentInput = Right;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        board_move_snake();
         system("clear");
         myBoard.print();
     }
